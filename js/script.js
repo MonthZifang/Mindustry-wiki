2139722038b81ccd548d029f81ddf0c2283d1bdd
MonthZifang <143945531+MonthZifang@users.noreply.github.com>
2025-05-16 10:37:29 +0800

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,19 @@
+/* ========================================================== 
+             js框架，对框架加载等基础服务提供支持
+                禁止乱动，否则会导致网站错误！
+========================================================== */
+
+class HeaderComponent extends HTMLElement {// 链接<app-header></app-header>，实现加载框架frame.html
+  constructor() {
+    super();
+    fetch('../frame.html')
+      .then(res => res.text())
+      .then(html => this.innerHTML = html);
+  }
+}
+customElements.define('app-header', HeaderComponent);
+
+/*document.getElementById('theme-toggle').addEventListener('click', function() {
+    document.body.classList.toggle('dark-theme');
+    document.body.classList.toggle('light-theme');
+  });*/
